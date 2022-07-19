4eed9dd035383a7bcd447a7a18536d4105192fda
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2022-07-19 18:29:04 +0530

    Time: 2 ms (35.82%), Space: 42.2 MB (40.02%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,23 +1,23 @@
 class Solution {
     public String interpret(String command) {
-        String result = "";
-	int i = 0;
-	while (i <= command.length() - 1){
-		if (command.charAt(i) == '('){
-			if (command.charAt(i + 1) == 'a'){
-				result += "al";
-				i += 4;
-			}
-			else {
-				result += "o";
-				i += 2;
-			}
-		}
-		else {
-			result += "G";
-			i += 1;
-		}
-	}
-	return result;
+       String str="";
+        int i=0;
+
+        while(i<=command.length()-1){
+            if(command.charAt(i)=='('){
+                if(command.charAt(i+1)=='a'){
+                    str=str+"al";
+                    i=i+4;
+                } else if (command.charAt(i+1)==')') {
+                    str=str+"o";
+                    i=i+2;
+                }
+            }
+            else{
+                str=str+"G";
+                i=i+1;
+            }
+        }
+        return str;
     }
 }
