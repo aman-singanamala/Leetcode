f3fb1babfac18e504dc47df8420f1fb0b23d99df
aman-singanamala <84006448+aman-singanamala@users.noreply.github.com>
2022-07-27 22:38:44 +0530

    Time: 2 ms (24.83%), Space: 51.1 MB (24.20%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.java
+++ 566-reshape-the-matrix/566-reshape-the-matrix.java
@@ -1,25 +1,23 @@
 class Solution {
     public int[][] matrixReshape(int[][] mat, int r, int c) {
-        int row=mat.length;
-        int col = mat[0].length;
-        int[]a=new int[row*col];
+        int m= mat.length;
+        int n= mat[0].length;
+        int[] arr= new int[m*n];
+        int[][] ans = new int[r][c];
+        if(m*n!=r*c)
+            return mat;
         int k=0;
-        if(row*col!=r*c) return mat;
-        
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                a[k++]=mat[i][j];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j<n ; j++) {
+                arr[k++]= mat[i][j];
             }
         }
-        
-        int[][] newMat=new int[r][c];
-        int p=0;
-        for(int l=0;l<r;l++){
-            for(int m=0;m<c;m++){
-                newMat[l][m]=a[p++];
+        int z=0;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                ans[i][j]= arr[z++];
             }
         }
-        
-        return newMat;
+        return ans;
     }
 }
